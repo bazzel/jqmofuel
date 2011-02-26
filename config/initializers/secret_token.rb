7dfc28d142111472f2d0f05adeecabc8d5f1289e
Patrick Baselier <patrick.baselier@gmail.com>
2011-02-26 19:13:15 +0100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jqmofuel::Application.config.secret_token = '873416985feb7237a794f98712409a3fe32083f31434b246783d69dd4bad085cd895fc833f458d44d4bfba8f7e0ccbe157a9490ed2ebc07e36b9fad49972ff66'
