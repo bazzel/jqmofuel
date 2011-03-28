e8dc0c4a5f748287917d629dff54a505309cbd93
Patrick Baselier <patrick.baselier@gmail.com>
2011-03-28 22:38:37 +0200

    Syntax error...

--- config/schedule.rb
+++ config/schedule.rb
@@ -19,7 +19,7 @@
 
 # Learn more: http://github.com/javan/whenever
 
-every :day, :at => '4:30 am' do
+every 1.day, :at => '4:30 am' do
   # Periodically test the Exception Notifier
   command "wget -q http://efueling.nl/test_exception_notifier"
 end
