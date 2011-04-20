a72f010d8bf04b065fefc554504e12e81d9a75be
Patrick Baselier <patrick.baselier@gmail.com>
2011-04-20 22:36:54 +0200

    Put config.action_mailer.default_url_options in corresponding env files.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,8 +17,7 @@ Jqmofuel::Application.configure do
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
 
-  # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  # We moved this to config/environment.rb to keep configurable enviroment settings in one place.
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
